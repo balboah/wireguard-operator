7816b03d643a862b60b9c03f5459cbe283dede34
Johnny Bergström <johnny@klaudify.se>
2019-06-13 17:53:06 +0200

    fix metric counter for "remove from pool"
    Use Set instead of Add

--- cmd/wgo/metrics.go
+++ cmd/wgo/metrics.go
@@ -37,12 +37,16 @@ func (p *metricPool) Free(ip net.IP) error {
 	return nil
 }
 
+// Remove from the pool a.k.a allocate these IPs.
 func (p *metricPool) Remove(ips ...net.IP) error {
 	err := p.IPPool.Remove(ips...)
 	if err != nil {
 		return err
 	}
-	p.metrics.Add("allocated", int64(len(ips)))
+	total := expvar.Int{}
+	total.Set(int64(len(ips)))
+	// Assume this is the exact number, use Set instead of Add.
+	p.metrics.Set("allocated", &total)
 	return nil
 }
 
